8ea9c60df916280978443050f7c259a499caf5f7
DAlxbss <180160638+DAlxbss@users.noreply.github.com>
2025-08-30 13:28:54 +0800

    Update apiSites.ts

--- src/config/apiSites.ts
+++ src/config/apiSites.ts
@@ -1,22 +1,478 @@
 export const API_SITES = [
-  {
-    key: "heimuer",
-    api: "https://json.heimuer.xyz",
-    name: "黑木耳",
-  },
-  {
-    key: "wolong",
-    api: "https://wolongzyw.com",
-    name: "卧龙资源",
-  },
-  {
-    key: "jisu",
-    api: "https://jszyapi.com",
-    name: "极速资源"
-  },
-  {
-    key: "ffzy5",
-    api: "http://ffzy5.tv",
-    name: "非凡影视"
-  },
-];
+    {
+        key: "dyttzy",
+        api: "http://caiji.dyttzyapi.com",
+        name: "电影天堂资源"
+    },
+    {
+        key: "heimuer",
+        api: "https://json.heimuer.xyz",
+        name: "黑木耳"
+    },
+    {
+        key: "ruyi",
+        api: "http://cj.rycjapi.com",
+        name: "如意资源"
+    },
+    {
+        key: "bfzy",
+        api: "https://bfzyapi.com",
+        name: "暴风资源"
+    },
+    {
+        key: "tyyszy",
+        api: "https://tyyszy.com",
+        name: "天涯资源"
+    },
+    {
+        key: "ffzy5",
+        api: "http://ffzy5.tv",
+        name: "非凡影视"
+    },
+    {
+        key: "zy360",
+        api: "https://360zy.com",
+        name: "360资源"
+    },
+    {
+        key: "maotaizy",
+        api: "https://caiji.maotaizy.cc",
+        name: "茅台资源"
+    },
+    {
+        key: "wolong",
+        api: "https://wolongzyw.com",
+        name: "卧龙资源"
+    },
+    {
+        key: "jisu",
+        api: "https://jszyapi.com",
+        name: "极速资源"
+    },
+    {
+        key: "dbzy",
+        api: "https://dbzy.tv",
+        name: "豆瓣资源"
+    },
+    {
+        key: "mozhua",
+        api: "https://mozhuazy.com",
+        name: "魔爪资源"
+    },
+    {
+        key: "mdzy",
+        api: "https://www.mdzyapi.com",
+        name: "魔都资源"
+    },
+    {
+        key: "zuid",
+        api: "https://api.zuidapi.com",
+        name: "最大资源"
+    },
+    {
+        key: "yinghua",
+        api: "https://m3u8.apiyhzy.com",
+        name: "樱花资源"
+    },
+    {
+        key: "wujin",
+        api: "https://api.wujinapi.me",
+        name: "无尽资源"
+    },
+    {
+        key: "wwzy",
+        api: "https://wwzy.tv",
+        name: "旺旺短剧"
+    },
+    {
+        key: "ikun",
+        api: "https://ikunzyapi.com",
+        name: "iKun资源"
+    },
+    {
+        key: "lzi",
+        api: "https://cj.lziapi.com",
+        name: "量子资源站"
+    },
+    {
+        key: "xiaomaomi",
+        api: "https://zy.xmm.hk",
+        name: "小猫咪资源"
+    },
+    {
+        key: "hongniu",
+        api: "http://hongniuzy2.com",
+        name: "紅牛資源"
+    },
+    {
+        key: "lziV2",
+        api: "http://cj.lziapi.com",
+        name: "量子資源"
+    },
+    {
+        key: "openMovie",
+        api: "http://vod-demo.onrender.com",
+        name: "開放電影"
+    },
+    {
+        key: "highQuality",
+        api: "http://api.1080zyku.com",
+        name: "优质资源库"
+    },
+    {
+        key: "movie39",
+        api: "http://39kan.com",
+        name: "39影視"
+    },
+    {
+        key: "heaven",
+        api: "http://vipmv.cc",
+        name: "天堂资源"
+    },
+    {
+        key: "lehuo",
+        api: "http://lehootv.com",
+        name: "乐活影视"
+    },
+    {
+        key: "tangrenjieV1",
+        api: "http://tangrenjie.tv",
+        name: "唐人街"
+    },
+    {
+        key: "kudianV1",
+        api: "http://api.kuapi.cc",
+        name: "酷点资源"
+    },
+    {
+        key: "wolongV2",
+        api: "http://collect.wolongzyw.com",
+        name: "卧龙资源"
+    },
+    {
+        key: "kudianV2",
+        api: "http://kudian10.com",
+        name: "酷点资源"
+    },
+    {
+        key: "tangrenjieV2",
+        api: "http://tangrenjie.tv",
+        name: "唐人街"
+    },
+    {
+        key: "senlin",
+        api: "http://slapibf.com",
+        name: "森林资源"
+    },
+    {
+        key: "yingku",
+        api: "http://api.ykapi.net",
+        name: "影库资源网"
+    },
+    {
+        key: "kuaicheV1",
+        api: "http://caiji.kczyapi.com",
+        name: "快车资源"
+    },
+    {
+        key: "tantan",
+        api: "http://apittzy.com",
+        name: "探探资源"
+    },
+    {
+        key: "shandianV1",
+        api: "http://sdzyapi.com",
+        name: "閃電資源"
+    },
+    {
+        key: "jinying",
+        api: "http://jyzyapi.com",
+        name: "金鹰资源"
+    },
+    {
+        key: "guangsu",
+        api: "http://api.guangsuapi.com",
+        name: "光速资源"
+    },
+    {
+        key: "aosika",
+        api: "http://aosikazy.com",
+        name: "奥斯卡资源网"
+    },
+    {
+        key: "laoya",
+        api: "http://api.apilyzy.com",
+        name: "老鸭资源采集"
+    },
+    {
+        key: "ukuV1",
+        api: "http://api.ukuapi.com",
+        name: "U酷资源"
+    },
+    {
+        key: "beidou",
+        api: "http://m3u8.bdxzyapi.com",
+        name: "北斗星资源"
+    },
+    {
+        key: "yinghuaV2",
+        api: "http://m3u8.apiyhzy.com",
+        name: "樱花资源网"
+    },
+    {
+        key: "kudianV3",
+        api: "http://api.kuapi.cc",
+        name: "酷点资源"
+    },
+    {
+        key: "shandianV2",
+        api: "http://sdzyapi.com",
+        name: "闪电资源"
+    },
+    {
+        key: "feisuV1",
+        api: "http://feisuzy.com",
+        name: "飞速资源"
+    },
+    {
+        key: "kuaibo",
+        api: "http://www.kuaibozy.com",
+        name: "快播资源"
+    },
+    {
+        key: "baiduV2",
+        api: "http://api.apibdzy.com",
+        name: "百度资源"
+    },
+    {
+        key: "aidan",
+        api: "http://lovedan.net",
+        name: "艾旦影视"
+    },
+    {
+        key: "piaohuaV1",
+        api: "http://www.zzrhgg.com",
+        name: "飘花电影"
+    },
+    {
+        key: "wangmin",
+        api: "http://prinevillesda.org",
+        name: "网民电影"
+    },
+    {
+        key: "siwa",
+        api: "http://siwazyw.cc",
+        name: "丝袜资源"
+    },
+    {
+        key: "tiankong",
+        api: "http://m3u8.tiankongapi.com",
+        name: "天空資源"
+    },
+    {
+        key: "haiwaikanV1",
+        api: "http://haiwaikan.com",
+        name: "海外看資源"
+    },
+    {
+        key: "haiwaikanV2",
+        api: "https://haiwaikan.com",
+        name: "海外看|点播"
+    },
+    {
+        key: "vod360",
+        api: "https://360zy.com",
+        name: "360|点播"
+    },
+    {
+        key: "heimuerV2",
+        api: "https://www.heimuer.tv",
+        name: "黑木耳|点播"
+    },
+    {
+        key: "citong",
+        api: "http://ys9.cttv.vip",
+        name: "刺桐|点播"
+    },
+    {
+        key: "official",
+        api: "http://gwcms.cttv.vip",
+        name: "官网|点播"
+    },
+    {
+        key: "yeyu",
+        api: "https://yyff.540734621.xyz",
+        name: "业余|点播"
+    },
+    {
+        key: "huaweibaV2",
+        api: "https://hw8.live",
+        name: "华为吧|点播"
+    },
+    {
+        key: "xiaohuangren",
+        api: "https://iqyi.xiaohuangrentv.com",
+        name: "小黄人|点播"
+    },
+    {
+        key: "niuniu",
+        api: "https://api.niuniuzy.me",
+        name: "牛牛|点播"
+    },
+    {
+        key: "yaya",
+        api: "https://cj.yayazy.net",
+        name: "丫丫|点播"
+    },
+    {
+        key: "ukuV2",
+        api: "https://api.ukuapi.com",
+        name: "U酷|点播"
+    },
+    {
+        key: "haohua",
+        api: "https://hhzyapi.com",
+        name: "豪华|点播"
+    },
+    {
+        key: "jisuV2",
+        api: "https://jszyapi.com",
+        name: "极速|点播"
+    },
+    {
+        key: "sijiu",
+        api: "https://49zyw.com",
+        name: "四九|点播"
+    },
+    {
+        key: "suoni",
+        api: "https://suoniapi.com",
+        name: "索尼|点播"
+    },
+    {
+        key: "ikunV2",
+        api: "https://ikunzyapi.com",
+        name: "ikun|点播"
+    },
+    {
+        key: "feifanV2",
+        api: "http://cj.ffzyapi.com",
+        name: "非凡|点播"
+    },
+    {
+        key: "liangziV2",
+        api: "https://cj.lziapi.com",
+        name: "量子|点播"
+    },
+    {
+        key: "baofengV2",
+        api: "https://bfzyapi.com",
+        name: "暴风|点播"
+    },
+    {
+        key: "hongniuV2",
+        api: "https://www.hongniuzy2.com",
+        name: "红牛|点播"
+    },
+    {
+        key: "feisuV2",
+        api: "https://www.feisuzyapi.com",
+        name: "飞速|点播"
+    },
+    {
+        key: "kuaikan",
+        api: "https://www.kuaikan-api.com",
+        name: "快看|点播"
+    },
+    {
+        key: "xiongzhang",
+        api: "https://xzcjz.com",
+        name: "熊掌|点播"
+    },
+    {
+        key: "kuaicheV2",
+        api: "https://caiji.kczyapi.com",
+        name: "快车|点播"
+    },
+    {
+        key: "shandianV3",
+        api: "http://sdzyapi.com",
+        name: "闪电|点播"
+    },
+    {
+        key: "yinghuaV3",
+        api: "https://m3u8.apiyhzy.com",
+        name: "樱花|点播"
+    },
+    {
+        key: "wolongV3",
+        api: "https://collect.wolongzyw.com",
+        name: "卧龙|点播"
+    },
+    {
+        key: "piaohuaV2",
+        api: "http://www.ahjiuman.com",
+        name: "飘花|点播"
+    },
+    {
+        key: "tianyi",
+        api: "https://www.911ysw.top",
+        name: "天翼|点播"
+    },
+    {
+        key: "huya",
+        api: "https://www.huyaapi.com",
+        name: "虎牙|点播"
+    },
+    {
+        key: "baiduV3",
+        api: "https://api.apibdzy.com",
+        name: "百度|点播"
+    },
+    {
+        key: "piaoling",
+        api: "https://p2100.net",
+        name: "飘零|点播"
+    },
+    {
+        key: "wujinV2",
+        api: "https://api.wujinapi.com",
+        name: "无尽|点播"
+    },
+    {
+        key: "subo",
+        api: "https://subocaiji.com",
+        name: "速博|点播"
+    },
+    {
+        key: "moduV2",
+        api: "https://caiji.moduapi.cc",
+        name: "魔都|点播"
+    },
+    {
+        key: "zuidaV2",
+        api: "http://zuidazy.me",
+        name: "最大|点播"
+    },
+    {
+        key: "qihu",
+        api: "https://caiji.qhzyapi.com",
+        name: "奇虎|点播"
+    },
+    {
+        key: "huohu",
+        api: "https://hhzyapi.com",
+        name: "火狐|点播"
+    },
+    {
+        key: "xinlang",
+        api: "https://api.xinlangapi.com",
+        name: "新浪|点播"
+    },
+    {
+        key: "kuaiyun",
+        api: "https://www.kuaiyunzy.com",
+        name: "快云|点播"
+    }
+]
+    
